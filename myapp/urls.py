5a236814c164e53ea7b92d904298ae504bbe9000
SHINE SB <shine@SB.GLOBAL>
2024-10-03 10:46:16 +0630

    homePage

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .import views
 
 urlpatterns = [
+    path('home/',views.home,name='home'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+def home(request):
+    home = Home.objects.all().order_by('-id')
+    return render(request,'index.html',{'home':home})
